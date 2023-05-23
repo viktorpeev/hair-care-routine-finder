9a16e801d542c3ce094b90b2b5c45d44a2f080f4
Viktor <lulzsec123@yahoo.com>
2023-05-23 17:41:11 +0300

    implemented like product and save in localstorage functionality

--- src/Pages/Result/index.js
+++ src/Pages/Result/index.js
@@ -11,51 +11,125 @@ export const Result = () => {
     selectedAnswerQ5,
   } = useContext(QuizContext);
 
-  const [productRecomendation, setProductRecomendation] = useState([]);
+  const [productRecommendation, setProductRecommendation] = useState(() => {
+    const storedProductRecommendation = localStorage.getItem('productRecommendation');
+    return storedProductRecommendation ? JSON.parse(storedProductRecommendation) : [];
+  });
+  const [currentPage, setCurrentPage] = useState(1);
+  const [itemsPerPage] = useState(2);
+  const [likedItems, setLikedItems] = useState(() => {
+    const storedLikedItems = localStorage.getItem('likedItems');
+    return storedLikedItems ? JSON.parse(storedLikedItems) : [];
+  });
 
   useEffect(() => {
     fetch('https://jeval.com.au/collections/hair-care/products.json?page=1')
       .then(response => response.json())
       .then(data => {
         const tagProducts = data.products.filter(product =>
-          product.tags.includes(`type_${questions[0].answers[selectedAnswerQ1]}`) //check for q1 only in tags
-          ||
-          product.body_html.includes(`${questions[2].answers[selectedAnswerQ3]}`) //check for q3 only in tags
-          ||
-          product.body_html.includes((`${questions[1].answers[selectedAnswerQ2]}`)) //check for q2 only in descr
-          ||
-          product.body_html.includes((`${questions[3].answers[selectedAnswerQ4]}`)) //check for q4 only in descr
-          ||
-          product.body_html.includes((`${questions[4].answers[selectedAnswerQ5]}`)) //check for q2 only in descr
-          ||
-          product.title.includes(`${questions[2].answers[selectedAnswerQ3]}`) //check for q3 only in tags
-          ||
-          product.title.includes((`${questions[1].answers[selectedAnswerQ2]}`)) //check for q2 only in title
-          ||
-          product.title.includes((`${questions[3].answers[selectedAnswerQ4]}`)) //check for q4 only in title
-          ||
-          product.title.includes((`${questions[4].answers[selectedAnswerQ5]}`)) //check for q2 only in title
+          product.tags.includes(`type_${questions[0].answers[selectedAnswerQ1]}`) ||
+          product.body_html.includes(`${questions[2].answers[selectedAnswerQ3]}`) ||
+          product.body_html.includes((`${questions[1].answers[selectedAnswerQ2]}`)) ||
+          product.body_html.includes((`${questions[3].answers[selectedAnswerQ4]}`)) ||
+          product.body_html.includes((`${questions[4].answers[selectedAnswerQ5]}`)) ||
+          product.title.includes(`${questions[2].answers[selectedAnswerQ3]}`) ||
+          product.title.includes((`${questions[1].answers[selectedAnswerQ2]}`)) ||
+          product.title.includes((`${questions[3].answers[selectedAnswerQ4]}`)) ||
+          product.title.includes((`${questions[4].answers[selectedAnswerQ5]}`))
         );
-        setProductRecomendation(tagProducts);
+        if (tagProducts.length > 0) {
+          setProductRecommendation(tagProducts);
+          localStorage.setItem('productRecommendation', JSON.stringify(tagProducts));
+        }
+
       })
       .catch(error => console.log(error));
   }, [selectedAnswerQ1, selectedAnswerQ2, selectedAnswerQ3, selectedAnswerQ4, selectedAnswerQ5, questions]);
 
+  // Calculate the indexes of the first and last item to display on the current page
+  const indexOfLastItem = currentPage * itemsPerPage;
+  const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+  const likedItemIds = likedItems.map(item => item.id);
+  const likedItemsPerPage = likedItems.filter(item => productRecommendation.find(product => product.id === item.id));
+  const remainingItems = productRecommendation.filter(item => !likedItemIds.includes(item.id));
+  const currentItems = [...likedItemsPerPage, ...remainingItems].slice(indexOfFirstItem, indexOfLastItem);
+
+
+  // Change the page
+  const paginate = pageNumber => {
+    setCurrentPage(pageNumber);
+  };
+
+  // Handle item like/unlike
+  const handleLike = (itemId) => {
+    if (likedItems.find(item => item.id === itemId)) {
+      // Unlike item
+      setLikedItems(likedItems.filter(item => item.id !== itemId));
+    } else {
+      // Like item
+      const likedItem = productRecommendation.find(item => item.id === itemId);
+      if (likedItem) {
+        setLikedItems([...likedItems, { id: likedItem.id, status: 'liked', title: likedItem.title }]);
+      }
+    }
+  };
+
+  const handleSubmit = () =>{
+    localStorage.clear();
+  }
+
+  useEffect(() => {
+    // Update local storage when liked items change
+    localStorage.setItem('likedItems', JSON.stringify(likedItems));
+  }, [likedItems]);
 
   return (
     <div>
-      {productRecomendation.length > 0 ? (
+      {currentItems.length > 0 ? (
         <div>
-          <h1>Recomended Products:</h1>
+          <h1>Recommended Products:</h1>
           <ul>
-            {productRecomendation.map(product => (
-              <li key={product.id}>{product.title}</li>
+            {currentItems.map(product => (
+              <li key={product.id}>
+                {product.title}
+                <button onClick={() => handleLike(product.id)}>
+                  {likedItems.find(item => item.id === product.id) ? 'Unlike' : 'Like'}
+                </button>
+              </li>
             ))}
           </ul>
+          <Pagination itemsPerPage={itemsPerPage} totalItems={productRecommendation.length} paginate={paginate} />
+          <button onClick={handleSubmit}>krai</button>
         </div>
       ) : (
         <div>No products found.</div>
       )}
     </div>
   );
-}
+};
+
+// Pagination component
+const Pagination = ({ itemsPerPage, totalItems, paginate }) => {
+  const pageNumbers = [];
+
+  // Calculate the total number of pages
+  const totalPages = Math.ceil(totalItems / itemsPerPage);
+
+  for (let i = 1; i <= totalPages; i++) {
+    pageNumbers.push(i);
+  }
+
+  return (
+    <nav>
+      <ul className="pagination">
+        {pageNumbers.map(number => (
+          <li key={number} className="page-item">
+            <button className="page-link" onClick={() => paginate(number)}>
+              {number}
+            </button>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+};
