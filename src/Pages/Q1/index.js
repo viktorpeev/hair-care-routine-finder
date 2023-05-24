41f57fddec671fe39aa1a0350bfe580868536a0c
Viktor <lulzsec123@yahoo.com>
2023-05-24 12:05:38 +0300

    implemented highlight functionality to question one

--- src/Pages/Q1/index.js
+++ src/Pages/Q1/index.js
@@ -1,8 +1,9 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect } from 'react';
 import { QuizContext } from '../../QuizContext';
 import { Link } from 'react-router-dom';
 import { Validation } from '../../Components/Validation';
 import { useNavigate } from 'react-router-dom';
+import './styles.scss';
 
 export const Q1 = () => {
   const {
@@ -19,14 +20,27 @@ export const Q1 = () => {
     await Validation(selectedAnswerQ1,2,navigate)
   };
 
+  useEffect(()=>{console.log(selectedAnswerQ1)},[selectedAnswerQ1])
+
+  const handleLiClick = (index) => {
+    const inputId = `input-${index}`;
+    const inputElement = document.getElementById(inputId);
+    if (inputElement) {
+      inputElement.click();
+    }
+  };
+
   return (
-    <div>
-      <h1>{currentQuestion.question}</h1>
-      <ul>
+    <div className='questionOne'>
+      <h1 className='questionOne--heading'>{currentQuestion.question}</h1>
+      <ul className='questionOne__answerList'>
         {currentQuestion.answers.map((answer, index) => (
-          <li key={index}>
-            <label style={{ textTransform: 'capitalize' }}>
+          <li key={index} onClick={() => handleLiClick(index)}
+          className={selectedAnswerQ1.includes(index) ? 'selected' : ''}
+          >
+            <label htmlFor={`input-${index}`} style={{ textTransform: 'capitalize' }}>
               <input
+                id={`input-${index}`}
                 type="radio"
                 value={index}
                 onChange={() => setSelectedAnswerQ1([index])}
