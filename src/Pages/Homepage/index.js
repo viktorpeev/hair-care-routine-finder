66f97b79bbb066e371d7f50ca7578af923005e8a
Viktor <lulzsec123@yahoo.com>
2023-05-24 11:17:14 +0300

    applied homepage styles

--- src/Pages/Homepage/index.js
+++ src/Pages/Homepage/index.js
@@ -1,12 +1,14 @@
 import { Link } from "react-router-dom";
 import './styles.scss';
 
-export const Homepage =()=> {
+export const Homepage = () => {
   return (
     <div className='homepage'>
-      <h1 className='homepage--heading'>Build a self care routine suitable for you</h1>
-      <p className='homepage--description'>Take out test to get a personalised self care routine based on your needs.</p>
-     <Link to='/question-1'>Start the quiz</Link>
-  </div>
+        <h1 className='homepage--heading'>Build a self care routine suitable for you</h1>
+        <p className='homepage--description'>Take out test to get a personalised self care routine based on your needs.</p>
+        <Link className="homepage__button" to='/question-1'>
+          <span className='homepage__button--text'>Start the quiz</span>
+        </Link>
+    </div>
   );
 }
